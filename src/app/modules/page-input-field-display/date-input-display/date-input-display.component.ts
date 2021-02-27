4e067707f731ee86257aebdc5dec227dc1e44075
Jonathan Rivas <jonathan@smartstart.us>
2021-02-27 17:03:32 +0800

    update datetime picker

--- src/app/modules/page-input-field-display/date-input-display/date-input-display.component.ts
+++ src/app/modules/page-input-field-display/date-input-display/date-input-display.component.ts
@@ -9,7 +9,10 @@ import { ElementValues } from '../../elementTools/interfaces/ElementValues';
 export class DateInputDisplayComponent implements OnInit {
   @Input() values: ElementValues;
   currentYear = new Date().getFullYear()
+  days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  allMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Oct", "Sep", "Nov", "Dec"];
   years = []
+  months = []
   constructor() {
     
    }
@@ -20,6 +23,17 @@ export class DateInputDisplayComponent implements OnInit {
     for (let index = 1920; index <= this.currentYear+1; index++) {
        defaultYears.unshift(index);
     }
+    let months = []
+    if (this.values.data.customMonths.length > 0) {
+    for (let month = 0; month < this.allMonths.length; month++) {
+      if (this.values.data.customMonths.includes(this.allMonths[month])) {
+        months.push(month+1)
+      }
+    }
+  } else {
+    months = [1,2,3,4,5,6,7,8,9,10,11,12]
+  }
+    this.months = months;
     this.years = this.years.length > 0? this.years.sort(function(a, b){return b - a}) : defaultYears;
   }
 
