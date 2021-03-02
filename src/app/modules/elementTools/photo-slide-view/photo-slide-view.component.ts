3d523141f7ad31ffb4f0ce0d3a1811cff0d7a904
Jonathan Rivas <jonathan@smartstart.us>
2021-03-03 01:00:40 +0800

    update sliding images

--- src/app/modules/elementTools/photo-slide-view/photo-slide-view.component.ts
+++ src/app/modules/elementTools/photo-slide-view/photo-slide-view.component.ts
@@ -10,6 +10,7 @@ export class PhotoSlideViewComponent implements OnInit, AfterViewInit {
   @Input() images: any[] = [];
   @Input() parent: string;
   @ViewChild('slides' , {static: false}) slides: IonSlides;
+  forward = true;
   slideOpts = {
     initialSlide: 1,
     speed: 400
@@ -24,32 +25,16 @@ export class PhotoSlideViewComponent implements OnInit, AfterViewInit {
     setTimeout(() => {
       if (this.images) {
         if (this.slides) {
-          let d = 1;
-          let count = 0;
           setInterval(() => {
-            console.log(count);
-            
-            this.slides.slideTo(count += d, 1800);
-            if (count == this.images.length || count < 1) {
-              d = -d;
+            if (this.forward) {
+            this.slides.slideNext(1800);
+            } else {
+              this.slides.slidePrev(1800);
             }
           }, 3000)
         }
       }
     }, 500);
-  }
-
-  start() {
-    let count = 0;
-    setInterval(function () {
-      console.log(count);
-
-      this.slides.slideTo(count++, 1500);
-      if (count == this.images.length) {
-        count = 0;
-      }
-    }, 1000)
-
   }
 
 }
